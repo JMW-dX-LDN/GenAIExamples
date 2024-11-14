b49c0b9cece4836a6d8c62732e2e86e877c4b91b
Kel Phillipson <kel@kelfish.com>
2024-11-14 07:55:08 -0800

    New env vars to configure the chatqna react app

--- ChatQnA/ui/react/src/config.ts
+++ ChatQnA/ui/react/src/config.ts
@@ -3,3 +3,8 @@
 
 export const DATA_PREP_URL = import.meta.env.VITE_DATA_PREP_SERVICE_URL;
 export const CHAT_QNA_URL = import.meta.env.VITE_BACKEND_SERVICE_ENDPOINT;
+export const LLM_MODEL_ID = import.meta.env.VITE_LLM_MODEL_ID;
+export const SET_SYSTEM_PROMPT =
+  import.meta.env.VITE_SET_SYSTEM_PROMPT === 'true';
+export const HIDE_UPLOAD_BUTTON =
+  import.meta.env.VITE_HIDE_UPLOAD_BUTTON === 'true';
